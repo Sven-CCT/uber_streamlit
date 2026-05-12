747fb9df479d935cb2dacbf39051a984430d0e15
Sven-CCT <sba25167@student.cct.ie>
2026-05-12 19:02:35 +0100

    Sixth commit

--- streamlit.py
+++ streamlit.py
@@ -22,3 +22,8 @@ data_load_state.text("Loading Data Done!!")
 
 st.subheader("Raw Data")
 st.write(data)
+
+st.subheader("Num pickups per hour")
+
+hist_values=np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
