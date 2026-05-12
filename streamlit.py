ef0cfaa7a4f86b837e9d3a692d96cdb442eb177d
Sven-CCT <sba25167@student.cct.ie>
2026-05-12 18:46:24 +0100

    Third commit

--- streamlit.py
+++ streamlit.py
@@ -2,4 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("Uber Pickups in New York City")
+st.title("Uber Pickups in New York")
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
